f265cb80e466825eafd4d4e2ed6ac5dc7ea07ca2
chalos67 <chalosampini@hotmail.com>
2026-05-31 01:29:54 -0300

    secciones conectadas y funcionando

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import home,profesores,conocenos,contactos
 
 urlpatterns = [
 
-    path("", home, name = "hombe"),
+    path("", home, name = "home"),
+    path("profesores/", profesores, name = "profesores" ),
+    path("conocenos/",conocenos, name= "conocenos"),
+    path("contactos/", contactos, name="contactos")
+   
 ]
--- web/views.py
+++ web/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def home(request):
     return render (request, "web/home.html")
+
+def profesores(request):
+    return render(request, "web/profesores.html")
+
+def conocenos(request):
+    return render(request, "web/conocenos.html")
+
+def contactos(request):
+    return render(request, "web/contactos.html")
