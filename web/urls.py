475d7b1d1529c64e3d1bc1e32b01f7c91a6f5c4b
chalos67 <chalosampini@hotmail.com>
2026-08-04 02:05:05 -0300

    Finzaliza landing v1.1

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home,actividades,historia,galeria
+from .views import home,actividades,historia,galeria,sponsors
 
 urlpatterns = [
 
     path("", home, name = "home"),
     path("actividades/",actividades, name = "profesores" ),
     path("historia/",historia, name= "historia"),
-    path("galeria/",galeria, name="galeria")
+    path("galeria/",galeria, name="galeria"),
+    path("sponsors/",sponsors, name="sponsors")
 ]
